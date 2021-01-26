f9bdb29da89b16ffbbcad3698e9af4df91d8f8f6
Felix <Felix@hh.de>
2021-01-26 11:25:06 +0100

    Add randomStudent method

--- src/test/java/de/neuefische/DB/StudentDBTest.java
+++ src/test/java/de/neuefische/DB/StudentDBTest.java
@@ -10,11 +10,11 @@ class StudentDBTest {
     @Test
     void listShouldReturnAllStudents(){
         // GIVEN
-        Student student1 = new Student("Peter" 1);
-        Student student3 = new Student("Maria" 2);
+        Student student1 = new Student("Peter", 1);
+        Student student3 = new Student("Maria", 2);
 
         Student[] students = {student1, student3};
-        StudentDB studentDB = new StudentDB(students)
+        StudentDB studentDB = new StudentDB(students);
 
     }
 
